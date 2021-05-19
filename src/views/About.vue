8555d9de544988f3eedc275c4862bcf3c011c315
Shane Dela Rosa <delarosa.shane19@gmail.com>
2021-05-19 23:15:33 +0800

    Vue 3 Sample App

---
+++ src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="about">
+    <h3>Version 1.0.0</h3>
+    <router-link to="/">Go Back</router-link>
+  </div>
+</template>
+
+<style scoped>
+.about {
+  text-align: center;
+}
+</style>
